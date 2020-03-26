61ee33bc105c2b4507ff2a8a77aa50a694415b62
Merlin Cox <merlin.cox@perkbox.com>
2020-03-26 16:59:50 +0000

    Refactor into packages

--- main.go
+++ main.go
@@ -1,274 +1,72 @@
 package main
 
 import (
-	"errors"
 	"flag"
-	"fmt"
-	"go/token"
-	"go/types"
 	"log"
 	"os"
 	"path/filepath"
-	"sort"
 	"strings"
-	"text/template"
-	"unicode"
-	"unicode/utf8"
 
-	"golang.org/x/tools/go/packages"
-)
-
-type commaStrings []string
-
-const outputFileName = "output.proto"
-
-func (i *commaStrings) String() string {
-	return ""
-}
-
-func (i *commaStrings) Set(value string) error {
-	*i = strings.Split(value, ",")
-	return nil
-}
-
-var (
-	filter       = flag.String("filter", "", "Filter by struct names. Case insensitive.")
-	targetFolder = flag.String("f", ".", "Protobuf output directory path.")
-	useTags      = flag.Bool("t", false, "Add import tagger/tagger.proto and write tag extensions if any of the structs are tagged.")
-	pkgPaths     commaStrings
+	"github.com/merlincox/go2proto/pkg/parser"
+	"github.com/merlincox/go2proto/pkg/protobuf"
 )
 
 func main() {
-	flag.Var(&pkgPaths, "p", `Comma-separated paths of packages to analyse. Relative paths ("./example/in") are allowed.`)
+
+	inputPathsString := flag.String("p", "", `Comma-separated paths of package directories to analyse for structs. Relative paths ("./example/in") are allowed.`)
+	structFilter := flag.String("f", "", "Filter by struct names.")
+	outputPath := flag.String("o", "./output.proto", "Protobuf output file path. The output directory must exist.")
+	useTags := flag.Bool("t", false, "Add import tagger/tagger.proto and write tag extensions if any of the structs are tagged.")
+
 	flag.Parse()
 
-	if len(pkgPaths) == 0 {
+	if len(*inputPathsString) == 0 {
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
 
-	//ensure the output path exists and is a directory
-	info, err := os.Stat(*targetFolder)
+	inputPaths := strings.Split(*inputPathsString, ",")
+
+	outputFolder := filepath.Dir(*outputPath)
+
+	//ensure the output directory exists and is a directory
+	info, err := os.Stat(outputFolder)
 
 	if os.IsNotExist(err) {
-		log.Fatalf("output folder %s does not exist", *targetFolder)
+		log.Fatalf("Output folder %s does not exist", outputFolder)
 	}
 
 	if info.Mode().IsRegular() {
-		log.Fatalf("%s is not a directory", *targetFolder)
+		log.Fatalf("%s is not a directory", outputFolder)
 	}
 
-	absTargetPath, err := filepath.Abs(*targetFolder)
+	absOutputFilePath, err := filepath.Abs(*outputPath)
 
 	if err != nil {
-		log.Fatalf("error getting absolute output folder: %s", err)
+		log.Fatalf("Error getting absolute output path: %s", err)
 	}
 
-	pkgs, err := loadPackages(pkgPaths)
+	msgs, err := parser.GetMessages(inputPaths, *structFilter)
+
 	if err != nil {
-		log.Fatalf("error fetching packages: %s", err)
+		log.Fatalf("Error getting messages: %s", err)
 	}
 
-	msgs := getMessages(pkgs, strings.ToLower(*filter))
-
-	if err = writeOutput(msgs, *targetFolder, *useTags); err != nil {
-		log.Fatalf("error writing output: %s", err)
+	if len(msgs) == 0 {
+		log.Fatalf("No messages were found")
 	}
 
-	log.Printf("output file written to %s\n", filepath.Join(absTargetPath, outputFileName))
-}
+	fout, err := os.Create(absOutputFilePath)
 
-// attempt to load all packages
-func loadPackages(pkgs []string) ([]*packages.Package, error) {
-
-	pwd, err := os.Getwd()
 	if err != nil {
-		return nil, fmt.Errorf("error getting working directory: %s", err)
+		log.Fatalf("Unable to create file %s : %s", absOutputFilePath, err)
 	}
 
-	fset := token.NewFileSet()
-	cfg := &packages.Config{
-		Dir:  pwd,
-		Mode: packages.LoadSyntax,
-		Fset: fset,
-	}
-	packages, err := packages.Load(cfg, pkgs...)
-	if err != nil {
-		return nil, err
-	}
-	var errs = ""
-	//check each loaded package for errors during loading
-	for _, p := range packages {
-		if len(p.Errors) > 0 {
-			errs += fmt.Sprintf("error fetching package %s: ", p.String())
-			for _, e := range p.Errors {
-				errs += e.Error()
-			}
-			errs += "; "
-		}
-	}
-	if errs != "" {
-		return nil, errors.New(errs)
-	}
-	return packages, nil
-}
+	defer fout.Close()
 
-type message struct {
-	Name   string
-	Fields []*field
-}
-
-type field struct {
-	Name       string
-	TypeName   string
-	Order      int
-	IsRepeated bool
-	Tags       string
-}
-
-func getMessages(pkgs []*packages.Package, filter string) []*message {
-	var out []*message
-	seen := map[string]struct{}{}
-	for _, p := range pkgs {
-		for _, t := range p.TypesInfo.Defs {
-			if t == nil {
-				continue
-			}
-			if !t.Exported() {
-				continue
-			}
-			if _, ok := seen[t.Name()]; ok {
-				continue
-			}
-			if s, ok := t.Type().Underlying().(*types.Struct); ok {
-				seen[t.Name()] = struct{}{}
-				if filter == "" || strings.Contains(strings.ToLower(t.Name()), filter) {
-					out = appendMessage(out, t, s)
-				}
-			}
-		}
-	}
-	sort.Slice(out, func(i, j int) bool { return out[i].Name < out[j].Name })
-	return out
-}
-
-func appendMessage(out []*message, t types.Object, s *types.Struct) []*message {
-	msg := &message{
-		Name:   t.Name(),
-		Fields: []*field{},
+	if err = protobuf.WriteOutput(fout, msgs, *useTags); err != nil {
+		log.Fatalf("Error writing output: %s", err)
 	}
 
-	for i := 0; i < s.NumFields(); i++ {
-		f := s.Field(i)
-		if !f.Exported() {
-			continue
-		}
-		newField := &field{
-			Name:       toProtoFieldName(f.Name()),
-			TypeName:   toProtoFieldTypeName(f),
-			IsRepeated: isRepeated(f),
-			Order:      i + 1,
-			Tags:       s.Tag(i),
-		}
-		msg.Fields = append(msg.Fields, newField)
-	}
-	out = append(out, msg)
-	return out
-}
-
-func toProtoFieldTypeName(f *types.Var) string {
-
-	switch f.Type().Underlying().(type) {
-	case *types.Basic:
-		name := f.Type().String()
-		return protobufType(name)
-	case *types.Slice, *types.Pointer, *types.Struct:
-		name := simpleName(f)
-		return protobufType(name)
-	default:
-		return f.Type().String()
-	}
-}
-
-func simpleName(f *types.Var) string {
-	return strings.TrimPrefix(strings.TrimLeft(f.Type().String(), "[]*"), f.Pkg().Path()+".")
-}
-
-func protobufType(name string) string {
-	switch name {
-	case "int":
-		return "int64"
-	case "float32":
-		return "float"
-	case "float64":
-		return "double"
-	default:
-		return name
-	}
-}
-
-func isRepeated(f *types.Var) bool {
-	_, ok := f.Type().Underlying().(*types.Slice)
-	return ok
-}
-
-func toProtoFieldName(name string) string {
-	if len(name) == 2 {
-		return strings.ToLower(name)
-	}
-	r, n := utf8.DecodeRuneInString(name)
-	return string(unicode.ToLower(r)) + name[n:]
-}
-
-func writeOutput(msgs []*message, path string, useTags bool) error {
-
-	protobufTemplate := `{{- define "field" }}{{.TypeName}} {{.Name}} = {{.Order}}{{if writeTags . }} [(tagger.tags) = "{{escapeQuotes .Tags}}"]{{ end }};{{ end -}}
-syntax = "proto3";
-
-package proto;
-{{- if importTagger}}
-
-import "tagger/tagger.proto";{{end}}
-{{range .}}
-message {{.Name}} {
-{{- range .Fields}}
-  {{ if .IsRepeated}}repeated {{ end }}{{ template "field" . }}
-{{- end}}
-}
-{{end}}
-`
-	f, err := os.Create(filepath.Join(path, outputFileName))
-	if err != nil {
-		return fmt.Errorf("unable to create file %s : %s", outputFileName, err)
-	}
-	defer f.Close()
-
-	customFuncMap := template.FuncMap{
-		"escapeQuotes": func(tag string) string {
-			return strings.Replace(tag, `"`, `\"`, -1)
-		},
-		"writeTags": func(f field) bool {
-			return useTags && f.Tags != ""
-		},
-		"importTagger": func() bool {
-			if !useTags {
-				return false
-			}
-			for _, msg := range msgs {
-				for _, field := range msg.Fields {
-					if field.Tags != "" {
-						return true
-					}
-				}
-			}
-			return false
-		},
-	}
-
-	tmpl, err := template.New("protobuf").Funcs(customFuncMap).Parse(protobufTemplate)
-	if err != nil {
-		panic(err)
-	}
-
-	return tmpl.Execute(f, msgs)
+	log.Printf("Output file written to %s\n", absOutputFilePath)
 }
---
+++ pkg/parser/parser.go
@@ -0,0 +1,106 @@
+package parser
+
+import (
+	"errors"
+	"fmt"
+	"go/token"
+	"go/types"
+	"os"
+	"sort"
+	"strings"
+
+	"golang.org/x/tools/go/packages"
+
+	"github.com/merlincox/go2proto/pkg/protobuf"
+)
+
+// GetMessages returns a sorted slice of protobuf message representations for the input paths and filter, or an error
+func GetMessages(inputPaths []string, filter string) ([]*protobuf.Message, error) {
+
+	pkgs, err := loadPackages(inputPaths)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var msgs []*protobuf.Message
+	seen := map[string]struct{}{}
+	for _, p := range pkgs {
+		for _, t := range p.TypesInfo.Defs {
+			if t == nil {
+				continue
+			}
+			if !t.Exported() {
+				continue
+			}
+			if _, ok := seen[t.Name()]; ok {
+				continue
+			}
+			if s, ok := t.Type().Underlying().(*types.Struct); ok {
+				seen[t.Name()] = struct{}{}
+				if filter == "" || strings.Contains(t.Name(), filter) {
+					msgs = appendMessage(msgs, t, s)
+				}
+			}
+		}
+	}
+	sort.Slice(msgs, func(i, j int) bool { return msgs[i].Name < msgs[j].Name })
+	return msgs, nil
+}
+
+// attempt to load all packages
+func loadPackages(inputPaths []string) ([]*packages.Package, error) {
+
+	pwd, err := os.Getwd()
+	if err != nil {
+		return nil, fmt.Errorf("error getting working directory: %s", err)
+	}
+
+	fset := token.NewFileSet()
+	cfg := &packages.Config{
+		Dir:  pwd,
+		Mode: packages.LoadSyntax,
+		Fset: fset,
+	}
+	packages, err := packages.Load(cfg, inputPaths...)
+	if err != nil {
+		return nil, err
+	}
+
+	var errs []string
+	//check each loaded package for errors during loading
+	for _, p := range packages {
+		if len(p.Errors) > 0 {
+			var perrs []string
+			for _, e := range p.Errors {
+				perrs = append(perrs, e.Error())
+			}
+			err := fmt.Sprintf("package %s: %s", p.String(), strings.Join(perrs, ", "))
+			errs = append(errs, err)
+		}
+	}
+	if len(errs) != 0 {
+		return nil, errors.New(strings.Join(errs, "; "))
+	}
+	return packages, nil
+}
+
+func appendMessage(out []*protobuf.Message, t types.Object, s *types.Struct) []*protobuf.Message {
+
+	msg := &protobuf.Message{
+		Name:   t.Name(),
+		Fields: []*protobuf.Field{},
+	}
+
+	order := 0
+	for i := 0; i < s.NumFields(); i++ {
+		goField := s.Field(i)
+		if !goField.Exported() {
+			continue
+		}
+		order++
+		msg.Fields = append(msg.Fields, protobuf.NewField(goField, order, s.Tag(i)))
+	}
+	out = append(out, msg)
+	return out
+}
